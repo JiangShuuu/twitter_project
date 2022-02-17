58068600dab228237cbc41d156162afc5543bcda
JiangShuuu <globelex65@gmail.com>
2022-02-17 22:10:49 +0800

    first commit

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="contain">
+    <i class="fa-solid fa-poo"></i>
+    <h1>404 Page Not Found</h1>
+    <i class="fa-solid fa-poo"></i>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@import "./../assets/scss/All.scss";
+.contain {
+  @include flexCenter;
+  height: 100vh;
+}
+</style>
