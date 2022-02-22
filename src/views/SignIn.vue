94b2956b84570e95a845d823caca3301ced7be9e
JiangShuuu <globelex65@gmail.com>
2022-02-22 10:38:33 +0800

    add form

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,17 +1,29 @@
 <template>
   <div class="signInPage">
-    <div class="title">
-      <img src="../assets/logo.png" alt="" />
-      <span class="title">登入 Alphitter</span>
+    <div class="content">
+      <div class="title">
+        <img src="../assets/logo.png" alt="" />
+        <span class="title">登入 Alphitter</span>
+      </div>
+      <form class="form">
+        <input
+          class="form-control form_account"
+          type="text"
+          placeholder="帳號"
+        />
+        <input
+          class="form-control form_password"
+          type="text"
+          placeholder="密碼"
+        />
+      </form>
+      <button type="button" class="btn">登入</button>
+      <div class="link">
+        <a href="#" class="link_signUp">註冊 Alphitter</a>
+        <div class="link_circle"></div>
+        <a href="#" class="link_admin">後台登入</a>
+      </div>
     </div>
-    <form class="form">
-      <input class="form-control form_account" type="text" placeholder="帳號" />
-      <input
-        class="form-control form_password"
-        type="text"
-        placeholder="密碼"
-      />
-    </form>
   </div>
 </template>
 
@@ -21,33 +33,67 @@
   @include flexCenter;
   flex-direction: column;
   margin-top: 60px;
-  .title {
-    @include flexCenter;
-    flex-direction: column;
-    font-size: 23px;
-    font-weight: 700;
-    margin-top: 20px;
-  }
-  .form {
-    margin-top: 40px;
-    input {
-      background: $light-gray;
-      border: none;
-      border-radius: 0;
-      border-bottom: 2px solid $mid-gray;
-      width: 540px;
-      height: 52px;
+  .content {
+    width: 540px;
+    .title {
+      @include flexCenter;
+      flex-direction: column;
+      font-size: 23px;
+      font-weight: 700;
+      margin-top: 20px;
     }
-    input::-webkit-input-placeholder {
-      color: $mid-gray;
-      font-size: 15px;
-      font-weight: 500;
-      text-align: left;
-      position: relative;
-      top: -0.8rem;
+    .form {
+      margin-top: 40px;
+      input {
+        background: $light-gray;
+        border: none;
+        border-radius: 0;
+        border-bottom: 2px solid $mid-gray;
+        width: 100%;
+        height: 52px;
+      }
+      input::-webkit-input-placeholder {
+        color: $mid-gray;
+        font-size: 15px;
+        font-weight: 500;
+        text-align: left;
+        position: relative;
+        top: -0.8rem;
+      }
+      &_password {
+        margin-top: 30px;
+      }
     }
-    &_password {
-      margin-top: 30px;
+    .btn {
+      background: $orange;
+      margin-top: 42px;
+      width: 100%;
+      border-radius: 50px;
+      color: white;
+      font-size: 18px;
+      font-weight: 700;
+    }
+    .link {
+      margin-top: 20px;
+      @include flexCenter;
+      justify-content: right;
+      a {
+        color: $link-blue;
+        font-weight: 700;
+        text-decoration: underline;
+        font-size: 18px;
+      }
+      span {
+        color: $link-blue;
+        margin: 0 0.25rem;
+      }
+      &_circle {
+        background-color: $link-blue;
+        width: 4px;
+        height: 4px;
+        border-radius: 50%;
+        margin: 0 0.25rem;
+      }
     }
   }
 }
