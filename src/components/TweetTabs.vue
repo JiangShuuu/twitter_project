90429c5b7318facf5d6caa95fe46e3fce1f71531
JiangShuuu <globelex65@gmail.com>
2022-02-24 10:38:23 +0800

    tab change & add otherPage

--- src/components/TweetTabs.vue
+++ src/components/TweetTabs.vue
@@ -1,21 +1,41 @@
 <template>
   <section class="tweet_tabs">
-    <router-link
-      to="/users/self"
-      :class="['tweet_tabs_btn', { active: this.router === '/users/self' }]"
-      >推文</router-link
-    >
-    <router-link
-      to="/users/reply"
-      :class="['tweet_tabs_btn', { active: this.router === '/users/reply' }]"
-      >推文與回覆</router-link
-    >
-    <router-link
-      to="/users/like"
-      :class="['tweet_tabs_btn', { active: this.router === '/users/like' }]"
-    >
-      喜歡的內容
-    </router-link>
+    <template v-if="isUsers">
+      <router-link
+        to="/users/self"
+        :class="['tweet_tabs_btn', { active: this.router === 'self' }]"
+        >推文</router-link
+      >
+      <router-link
+        to="/users/reply"
+        :class="['tweet_tabs_btn', { active: this.router === 'reply' }]"
+        >推文與回覆</router-link
+      >
+      <router-link
+        to="/users/like"
+        :class="['tweet_tabs_btn', { active: this.router === 'like' }]"
+      >
+        喜歡的內容
+      </router-link>
+    </template>
+    <template v-else>
+      <router-link
+        :to="{ name: 'other-self' }"
+        :class="['tweet_tabs_btn', { active: this.router === 'other-self' }]"
+        >推文</router-link
+      >
+      <router-link
+        :to="{ name: 'other-reply' }"
+        :class="['tweet_tabs_btn', { active: this.router === 'other-reply' }]"
+        >推文與回覆</router-link
+      >
+      <router-link
+        :to="{ name: 'other-like' }"
+        :class="['tweet_tabs_btn', { active: this.router === 'other-like' }]"
+      >
+        喜歡的內容
+      </router-link>
+    </template>
   </section>
 </template>
 
@@ -25,15 +45,26 @@ export default {
   data() {
     return {
       router: "",
+      isUsers: true,
     };
   },
   watch: {
     $route(to) {
-      this.router = to.path;
+      this.router = to.name;
     },
   },
   mounted() {
-    this.router = this.$route.path;
+    this.router = this.$route.name;
+    this.confirmRouter();
+  },
+  methods: {
+    confirmRouter() {
+      if (this.router.includes("other")) {
+        this.isUsers = false;
+      } else if (this.router.includes("users")) {
+        this.isUsers = true;
+      }
+    },
   },
 };
 </script>
---
+++ src/views/Other.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="users">
+    <div class="users_main">
+      <NavBar />
+      <div class="users_content">
+        <UserCard />
+        <TweetTabs />
+        <router-view />
+      </div>
+      <Popular />
+    </div>
+  </section>
+</template>
+
+<script>
+import NavBar from "../components/NavBar.vue";
+import Popular from "../components/Popular.vue";
+import UserCard from "../components/UserCard.vue";
+import TweetTabs from "../components/TweetTabs.vue";
+export default {
+  name: "Users",
+  components: {
+    UserCard,
+    TweetTabs,
+    NavBar,
+    Popular,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/All.scss";
+.users {
+  max-width: 1440px;
+  max-height: 1200px;
+  margin: 0 auto;
+  &_main {
+    /* 使用grid區分三份 */
+    display: grid;
+    grid-template-columns: 378px 600px 350px;
+    grid-auto-rows: 1200px;
+  }
+  &_content {
+    border: 1px solid $light-gray;
+    width: 600px;
+  }
+}
+</style>
