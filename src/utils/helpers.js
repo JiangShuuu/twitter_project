646f55925a5775b98d8144d664096d096289d755
yumimm <yuu9916@gmail.com>
2022-02-26 05:23:24 +0800

    feat: 新增axios環境設定

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,5 @@
 import Swal from "sweetalert2";
+import axios from 'axios'
 
 export const Toast = Swal.mixin({
   toast: true,
@@ -6,3 +7,24 @@ export const Toast = Swal.mixin({
   showConfirmButton: false,
   timer: 3000,
 });
+// axios
+const baseURL = 'https://protected-springs-71103.herokuapp.com/'
+
+const axiosInstance = axios.create({
+  baseURL
+})
+
+axiosInstance.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token')
+
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`
+    }
+
+    return config
+  },
+  err => Promise.reject(err)
+)
+
+export const apiHelper = axiosInstance
