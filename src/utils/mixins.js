b3af01bf7c333177fd75b3306bde30c702c3d10e
JiangShuuu <globelex65@gmail.com>
2022-02-26 11:50:51 +0800

    admin dummyData

---
+++ src/utils/mixins.js
@@ -0,0 +1,10 @@
+import moment from "moment";
+
+export const fromNowFilter = {
+  filters: {
+    fromNow(datatime) {
+      if (!datatime) return "-";
+      return moment(datatime).fromNow();
+    },
+  },
+};
