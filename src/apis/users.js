402249b6c875df34858f6fb17471c5e250f66230
JiangShuuu <globelex65@gmail.com>
2022-02-27 11:46:15 +0800

    setting usersAPI update

--- src/apis/users.js
+++ src/apis/users.js
@@ -3,7 +3,12 @@ const getToken = () => localStorage.getItem("token");
 
 export default {
   getCurrentUser() {
-    return apiHelper.get("api/users/current_user", {
+    return apiHelper.get("api/current_user", {
+      headers: { Authorization: `Bearer ${getToken()}` },
+    });
+  },
+  settingUserAccount(id) {
+    return apiHelper.put(`api/users/${id}/setting`, {
       headers: { Authorization: `Bearer ${getToken()}` },
     });
   },
