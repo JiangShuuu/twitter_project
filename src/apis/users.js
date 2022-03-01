8b1af9b17dee8dd2e97688a5c08e9eaf5fa2cc4d
JiangShuuu <globelex65@gmail.com>
2022-03-01 13:47:11 +0800

    add api interceptors

--- src/apis/users.js
+++ src/apis/users.js
@@ -1,63 +1,38 @@
 import { apiHelper } from "../utils/helpers";
-const getToken = () => localStorage.getItem("token");
 
 export default {
   getCurrentUser() {
-    return apiHelper.get("api/current_user", {
-      headers: { Authorization: `Bearer ${getToken()}` },
-    });
+    return apiHelper.get("api/current_user");
   },
   settingUserAccount(id, formData) {
-    return apiHelper.put(`api/users/${id}/setting`, formData, {
-      headers: { Authorization: `Bearer ${getToken()}` },
-    });
+    return apiHelper.put(`api/users/${id}/setting`, formData);
   },
   getUserInfo(id) {
-    return apiHelper.get(`api/users/${id}`, {
-      headers: { Authorization: `Bearer ${getToken()}` },
-    });
+    return apiHelper.get(`api/users/${id}`);
   },
   getUserTweet(id) {
-    return apiHelper.get(`api/users/${id}/tweets`, {
-      headers: { Authorization: `Bearer ${getToken()}` },
-    });
+    return apiHelper.get(`api/users/${id}/tweets`);
   },
   getUserReply(id) {
-    return apiHelper.get(`api/users/${id}/replied_tweets`, {
-      headers: { Authorization: `Bearer ${getToken()}` },
-    });
+    return apiHelper.get(`api/users/${id}/replied_tweets`);
   },
   getUserLikes(id) {
-    return apiHelper.get(`api/users/${id}/likes`, {
-      headers: { Authorization: `Bearer ${getToken()}` },
-    });
+    return apiHelper.get(`api/users/${id}/likes`);
   },
   getUsersTop() {
-    return apiHelper.get("api/users/top", {
-      headers: { Authorization: `Bearer ${getToken()}` },
-    });
+    return apiHelper.get("api/users/top");
   },
   // 追隨者頁面
   getUserFollow(id) {
-    return apiHelper.get(`api/users/${id}/followings`, {
-      headers: { Authorization: `Bearer ${getToken()}` },
-    });
+    return apiHelper.get(`api/users/${id}/followings`);
   },
   addFollow({ id }) {
-    return apiHelper.post("api/followships",{ id },
-      {
-        headers: { Authorization: `Bearer ${getToken()}` },
-      }
-    );
+    return apiHelper.post("api/followships", { id });
   },
   deleteFollow({ userId }) {
-    return apiHelper.delete(`api/followships/${userId}`, {
-      headers: { Authorization: `Bearer ${getToken()}` },
-    });
+    return apiHelper.delete(`api/followships/${userId}`);
   },
   editUserInfo(id, formData) {
-    return apiHelper.put(`api/users/${id}`, formData, {
-      headers: { Authorization: `Bearer ${getToken()}` },
-    });
+    return apiHelper.put(`api/users/${id}`, formData);
   },
 };
